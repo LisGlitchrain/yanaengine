5f9b4e55f0a5445e76852afd29f634cb16dee8b5
glitchrain <hlambdafox@gmail.com>
2026-07-27 09:11:32 +0300

    Added hpp/cpp scaffold files.

---
+++ engine/src/Engine.cpp
@@ -0,0 +1,15 @@
+#include "yana/engine/Engine.hpp"
+
+namespace yana::engine
+{
+Engine::Engine() = default;
+Engine::~Engine() = default;
+
+void Engine::Initialize()
+{
+}
+
+void Engine::Shutdown()
+{
+}
+} // namespace yana::engine
---
+++ sandbox/src/main.cpp
@@ -0,0 +1,7 @@
+#include "yana/application/Application.hpp"
+
+int main()
+{
+    yana::application::Application app;
+    return app.Run();
+}
